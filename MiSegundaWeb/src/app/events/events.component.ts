2fe0f173d62da41fe3e9268e800243b28235db1e
quimmp26 <72405737+quimmp26@users.noreply.github.com>
2020-10-29 17:58:55 +0100

    ngIf. Add, edit, remove forms
    Afegir Event i cambiar formularis segons accio.

--- MiSegundaWeb/src/app/events/events.component.ts
+++ MiSegundaWeb/src/app/events/events.component.ts
@@ -11,29 +11,48 @@ export class EventsComponent implements OnInit {
   events: EventGallery[] = [];
   accion: String;
 
+  public container: string = "add";
+
+  public evento: string;
+  public ubicacion: string;
+  public email: string;
+  public descripcion: string;
+  public imagen: string;
+
   constructor() { }
 
   ngOnInit(): void {
 
-    this.events.push(new EventGallery('Hooker Lake', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event1.jpg'));
-    this.events.push(new EventGallery('Hiking trail', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event2.jpg'));
-    this.events.push(new EventGallery('Wild river', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event3.jpg'));
-    this.events.push(new EventGallery('Mount Fuji', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event4.jpg'));
-    this.events.push(new EventGallery('Cascade Lauzon', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event5.jpg'));
-
+    this.events.push(new EventGallery('Rappel', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event1.jpg', 'Hooker Lake', 'quiim2000@gmail.com'));
+    this.events.push(new EventGallery('Caminata', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event2.jpg', 'Hiking trail', 'victor202@gmail.com'));
+    this.events.push(new EventGallery('Cayak', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event3.jpg', 'Wild river', 'quimmartinezpique@gmail.com'));
+    this.events.push(new EventGallery('Escalada', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event4.jpg', 'Mount Fuji', 'autor12@gmail.com'));
+    this.events.push(new EventGallery('Excursion Turistica', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event5.jpg', 'Cascade Lauzon', 'quiim2000@gmail.com'));
   }
 
   elegirAccion(event) {
     console.log(event.target.value);
     if(event.target.value == "add") {
       this.accion = "AÑADIR EVENTO";
+      this.container = "add"
     } else if(event.target.value == "edit") {
       this.accion = "EDITAR EVENTO";
+      this.container = "edit"
     } else if (event.target.value == "delete") {
+      this.container = "delete"
       this.accion = "ELIMINAR EVENTO";
     } else {
       this.accion = "";
     }
+  }
+
+  enviarDatosAdd(event, ubi, email, descrip) {
+    this.evento = event;
+    this.ubicacion = ubi;
+    this.email = email;
+    this.descripcion = descrip;
+
+    this.events.push(new EventGallery(this.evento, this.descripcion, 'assets/img-events/event1.jpg', this.ubicacion, this.email));
   }
 
 }
--- MiSegundaWeb/src/app/models/event-gallery.ts
+++ MiSegundaWeb/src/app/models/event-gallery.ts
@@ -1,6 +1,12 @@
 export class EventGallery {
 
-  constructor(public tituloEv: string, public descripEv: string, public imgEv: string){
+  constructor(
+    public tituloEv: string,
+    public descripEv: string,
+    public imgEv: string,
+    public ubicacionEv: string,
+    public emailAutor: string
+  ){
 
   }
 }
