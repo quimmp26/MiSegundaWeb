dc99ccc6fa6fc7da38ef071fdfe435a5c606518c
Quim <72405737+quimmp26@users.noreply.github.com>
2020-11-06 21:45:20 +0100

    modificar events

--- MiSegundaWeb/src/app/events/events.component.ts
+++ MiSegundaWeb/src/app/events/events.component.ts
@@ -1,4 +1,7 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+import { isDevMode, Output } from '@angular/core';
+import { Input } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { EventGallery } from '../models/event-gallery';
 
@@ -11,33 +14,24 @@ export class EventsComponent implements OnInit {
 
   events: EventGallery[] = [];
   accion: String;
-
   public container: string;
 
-  public evento: string;
-  public ubicacion: string;
-  public email: string;
-  public descripcion: string;
-  public imagen: string;
+  eventModificar: EventGallery = null;
 
-  public compEvent: boolean = true;
-  public compDescrip: boolean = true;
-  public compUbi: boolean = true;
-  public compEmail: boolean = true;
-  public compCheck: boolean = true;
-
-  public mensajeError: string;
+  id: number = null
 
   constructor() { }
 
   ngOnInit(): void {
 
-     this.events.push(new EventGallery('Rappel', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event1.jpg', 'Hooker Lake', 'quiim2000@gmail.com'));
-     this.events.push(new EventGallery('Caminata', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event2.jpg', 'Hiking trail', 'victor202@gmail.com'));
-     this.events.push(new EventGallery('Cayak', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event3.jpg', 'Wild river', 'quimmartinezpique@gmail.com'));
-     this.events.push(new EventGallery('Escalada', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event4.jpg', 'Mount Fuji', 'autor12@gmail.com'));
-     this.events.push(new EventGallery('Excursion Turistica', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event5.jpg', 'Cascade Lauzon', 'quiim2000@gmail.com'));
+     this.events.push(new EventGallery(Math.round(Math.random()*100),'Rappel', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event1.jpg', 'Hooker Lake', 'quiim2000@gmail.com'));
+     this.events.push(new EventGallery(Math.round(Math.random()*100),'Caminata', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event2.jpg', 'Hiking trail', 'victor202@gmail.com'));
+     this.events.push(new EventGallery(Math.round(Math.random()*100),'Cayak', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event3.jpg', 'Wild river', 'quimmartinezpique@gmail.com'));
+     this.events.push(new EventGallery(Math.round(Math.random()*100),'Escalada', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event4.jpg', 'Mount Fuji', 'autor12@gmail.com'));
+     this.events.push(new EventGallery(Math.round(Math.random()*100),'Excursion Turistica', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta odit aliquam neque? Dignissimos, blanditiis.', 'assets/img-events/event5.jpg', 'Cascade Lauzon', 'quiim2000@gmail.com'));
   }
+
+
 
   elegirAccion(event) {
     console.log(event.target.value);
@@ -55,51 +49,48 @@ export class EventsComponent implements OnInit {
     }
   }
 
-  enviarDatosAdd(event, ubi, email, descrip, img, check) {
+  anadirEvento(ev) {
 
-    this.compEvent = true;
-    this.compDescrip = true;
-    this.compUbi = true;
-    this.compEmail = true;
-    this.compCheck = true;
+    this.events.push(new EventGallery(ev.idEv ,ev.tituloEv , ev.descripEv, ev.imgEv, ev.ubicacionEv, ev.emailAutor));
 
-    if(!(event && ubi && email && descrip)){
-      this.mensajeError = "Los campos 'Evento', 'Ubicacion', 'Email' y 'Descripcion' deben estar rellenados";
-      this.compEvent = false;
-    }
-    else if(check == false) {
-      this.mensajeError = "Debes Aceptar condiciones para añadir el evento"
-      this.compCheck = false;
-    }
-    else if(descrip.length < 2){
-      this.mensajeError = "La descripcion debe ser de 2 o mas caracteres!"
-      this.compDescrip = false;
-    }
-    else if(!email.includes('@')){
-      this.mensajeError = "Debes añadir el simbolo @ a tu correo electronico!"
-      this.compEmail = false;
+  }
+
+  editarEvento(ev: EventGallery){
+
+
+    if(this.container == "edit"){
+
+      this.eventModificar = ev;
+
     }
 
-    if(this.compEvent && this.compCheck && this.compDescrip && this.compEmail){
-       this.events.push(new EventGallery(event, descrip, img, ubi, email));
+  }
+
+  editEvento(ev){
+
+    for(var i=0; i<this.events.length; i++){
+
+     console.log(this.events[i].idEv);
+
+        // this.events[i].tituloEv = ev.tituloEv;
+        // this.events[i].descripEv = ev.descripEv;
+        // this.events[i].imgEv = ev.imgEv;
+        // this.events[i].ubicacionEv = ev.ubicacionEv;
+        // this.events[i].emailAutor = ev.emailAutor;
+
     }
+    console.log(ev.idEv);
 
 
   }
 
 
   eliminarEvento(event){
-
     for(var j = 0; j<this.events.length; j++) {
-
       if(this.events[j].tituloEv == event){
-
         this.events.splice( j, 1 );
-
       }
-
     }
-
   }
 
 
--- MiSegundaWeb/src/app/models/event-gallery.ts
+++ MiSegundaWeb/src/app/models/event-gallery.ts
@@ -1,6 +1,7 @@
 export class EventGallery {
 
   constructor(
+    public idEv: number,
     public tituloEv: string,
     public descripEv: string,
     public imgEv: string,
