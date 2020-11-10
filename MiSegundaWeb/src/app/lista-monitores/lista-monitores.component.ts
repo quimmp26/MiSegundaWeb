3899e1894c1b81150dc587b2bcab6393acec98c8
Quim <72405737+quimmp26@users.noreply.github.com>
2020-11-10 17:18:43 +0100

    Final Versio1 projecte

--- MiSegundaWeb/src/app/lista-monitores/lista-monitores.component.ts
+++ MiSegundaWeb/src/app/lista-monitores/lista-monitores.component.ts
@@ -16,10 +16,11 @@ export class ListaMonitoresComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.monitor.push(new Monitor('Alejandro', 'García', 'Monitor de Senderismo', 'assets/img-monitors/1.png', 26));
-    this.monitor.push(new Monitor('Sandra', 'Gomez', 'Monitora Campamento Escolar', 'assets/img-monitors/2.jpg', 23));
+    this.monitor.push(new Monitor('Alejandro', 'García', 'Monitor de Senderismo', 'assets/img-monitors/1.jpg', 26));
+    this.monitor.push(new Monitor('Jorge', 'Gomez', 'Monitor Campamento Escolar', 'assets/img-monitors/2.jpg', 23));
     this.monitor.push(new Monitor('Juan', 'Martínez', 'Monitor de Escalada', 'assets/img-monitors/3.jpg', 32));
-    this.monitor.push(new Monitor('Jorge', 'Pérez', 'Monitor de Canoa', 'assets/img-monitors/4.jpg', 20));
+    this.monitor.push(new Monitor('Sandra', 'Pérez', 'Monitora de Canoa', 'assets/img-monitors/4.jpg', 20));
+    this.monitor.push(new Monitor('Marta', 'Sanchez', 'Monitora de Excursiones', 'assets/img-monitors/5.jpg', 22));
 
   }
 
